e06fd18b3a43180926f93f5b6192e558aebb2cc8
joshuadmorris <joshuadmorris@02f9dee6-7804-11de-adf2-cbd3851e0ccf>
2011-04-17 20:23:32 +0000

    Initial Import

---
+++ projects/commons/authorization/trunk/src/main/opstring/authorization.groovy
@@ -0,0 +1,29 @@
+import org.rioproject.config.Constants
+
+deployment(name:'Authorization') {
+    /* Configuration for the discovery group that the service should join.
+     * This first checks if the org.rioproject.groups property is set, if not
+     * the user name is used */
+    groups System.getProperty(Constants.GROUPS_PROPERTY_NAME,
+                              System.getProperty('user.name'))
+
+    /* Declares the artifacts required for deployment. Note the 'dl'
+     * classifier used for the 'download' jar */
+    artifact id:'service', 'com.unitt.commons.authorization:authorization-service:1.0-SNAPSHOT'
+    artifact id:'service-dl', 'com.unitt.commons.authorization:authorization-api:1.0-SNAPSHOT'
+
+    /*
+     * Declare the service to be deployed. The number of instances deployed
+     * defaults to 1. If you require > 1 instances change as needed
+     */
+    service(name: 'Authorization') {
+        interfaces {
+            classes 'com.unitt.commons.authorization.Authorization'
+            artifact ref:'service-dl'
+        }
+        implementation(class:'com.unitt.commons.authorization.AuthorizationImpl') {
+            artifact ref:'service'
+        }
+        maintain 1
+    }
+}
