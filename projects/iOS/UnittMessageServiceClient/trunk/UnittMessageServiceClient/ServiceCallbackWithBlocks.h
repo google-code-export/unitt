5c58af960be39bafb952124cf83af35e533b8ed5
joshuadmorris@gmail.com <joshuadmorris@gmail.com@02f9dee6-7804-11de-adf2-cbd3851e0ccf>
2011-12-12 02:41:25 +0000

    - added service callback with blocks

---
+++ projects/iOS/UnittMessageServiceClient/trunk/UnittMessageServiceClient/ServiceCallbackWithBlocks.h
@@ -0,0 +1,46 @@
+//
+//  ServiceCallbackWithBlocks.h
+//  UnittMessageServiceClient
+//
+//  Created by Josh Morris on 12/11/11.
+//  Copyright 2011 UnitT Software. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+//  use this file except in compliance with the License. You may obtain a copy of
+//  the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  License for the specific language governing permissions and limitations under
+//  the License.
+//
+
+
+#import <Foundation/Foundation.h>
+
+typedef void (^HandlesNoResult) ();
+typedef void (^HandlesResult) (id);
+typedef void (^HandlesError) (NSError*);
+
+@interface ServiceCallbackWithBlocks : NSObject {
+    HandlesNoResult onEmptyComplete;
+    HandlesResult onComplete;
+    HandlesResult onPartial;
+    HandlesError onError;
+}
+
+@property (nonatomic, copy) void (^onEmptyComplete)();
+@property (nonatomic, copy) void (^onComplete)(id);
+@property (nonatomic, copy) void (^onPartial)(id);
+@property (nonatomic, copy) void (^onError)(NSError*);
+
++ (id) callbackWithOnEmptyComplete:(HandlesNoResult) aOnComplete onError:(HandlesError) aOnError;
++ (id) callbackWithOnComplete:(HandlesResult) aOnComplete onPartial:(HandlesResult) aOnPartial onError:(HandlesError) aOnError;
+
+- (id) initWithOnEmptyComplete:(HandlesNoResult) aOnComplete onError:(HandlesError) aOnError;
+- (id) initWithOnComplete:(HandlesResult) aOnComplete onPartial:(HandlesResult) aOnPartial onError:(HandlesError) aOnError;
+
+@end
---
+++ projects/iOS/UnittMessageServiceClient/trunk/UnittMessageServiceClient/ServiceCallbackWithBlocks.m
@@ -0,0 +1,68 @@
+//
+//  ServiceCallbackWithBlocks.m
+//  UnittMessageServiceClient
+//
+//  Created by Josh Morris on 12/11/11.
+//  Copyright 2011 UnitT Software. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+//  use this file except in compliance with the License. You may obtain a copy of
+//  the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  License for the specific language governing permissions and limitations under
+//  the License.
+//
+
+
+#import "ServiceCallbackWithBlocks.h"
+
+
+@implementation ServiceCallbackWithBlocks
+@synthesize onComplete;
+@synthesize onPartial;
+@synthesize onError;
+@synthesize onEmptyComplete;
+
+
++ (id) callbackWithOnComplete:(HandlesResult) aOnComplete onPartial:(HandlesResult) aOnPartial onError:(HandlesError) aOnError {
+    return [[[[self class] alloc] initWithOnComplete:aOnComplete onPartial:aOnPartial onError:aOnError] autorelease];
+}
+
++ (id) callbackWithOnEmptyComplete:(HandlesNoResult) aOnComplete onError:(HandlesError) aOnError {
+    return [[[[self class] alloc] initWithOnEmptyComplete:aOnComplete onError:aOnError] autorelease];
+}
+
+- (id) initWithOnComplete:(HandlesResult) anOnComplete onPartial:(HandlesResult) anOnPartial onError:(HandlesError) anOnError {
+    self = [super init];
+    if (self) {
+        onComplete = [anOnComplete copy];
+        onPartial = [anOnPartial copy];
+        onError = [anOnError copy];
+    }
+
+    return self;
+}
+
+- (id) initWithOnEmptyComplete:(HandlesNoResult) anOnComplete onError:(HandlesError) anOnError {
+    self = [super init];
+    if (self) {
+        onEmptyComplete = [anOnComplete copy];
+        onError = [anOnError copy];
+    }
+
+    return self;
+}
+
+- (void) dealloc {
+    [onEmptyComplete release];
+    [onComplete release];
+    [onPartial release];
+    [onError release];
+    [super dealloc];
+}
+@end
